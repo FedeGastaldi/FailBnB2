1ebd473f0f7baf7d1733f760df3538c572ce9cb8
Enzo <enzodellape@gmail.com>
2025-05-16 12:22:40 -0300

    init: Configurar backend Express con ruta basica para empezar

---
+++ backend/models/db.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("conectado a la BDD");
+});
+module.exports = connection;
